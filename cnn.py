0d5c95e63571bcdacd6e89f856bfbb5e1c82b38a
MarcosGrzeca <marcos-a-g@hotmail.com>
2019-09-24 17:24:50 -0300

    Update cnn.py

--- cnn.py
+++ cnn.py
@@ -305,7 +305,8 @@ if __name__ == "__main__":
     print 'Allowing embedding learning: %s' %(str(LEARN_EMBEDDINGS))
 
     #word2vec_model = word2vec.KeyedVectors.load_word2vec_format(path_to_word2vec, binary=True)
-    word2vec_model = gensim.models.Word2Vec.load_word2vec_format(GLOVE_MODEL_FILE)
+    #word2vec_model = gensim.models.Word2Vec.load_word2vec_format(GLOVE_MODEL_FILE)
+    word2vec_model = gensim.models.KeyedVectors.load_word2vec_format(GLOVE_MODEL_FILE)
     np.random.seed(SEED)
 
 

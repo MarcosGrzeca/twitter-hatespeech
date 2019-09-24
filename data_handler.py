715988141792caf700ecfe607ed55096da164479
MarcosGrzeca <marcos-a-g@hotmail.com>
2019-09-24 20:11:27 -0300

    Update data_handler.py

--- data_handler.py
+++ data_handler.py
@@ -8,15 +8,15 @@ def get_data():
     files = ['ambos.json']
     for file in files:
         with codecs.open('./tweet_data/' + file, 'r', encoding='utf-8') as f:
-            data = f.readlines()
-        for line in data:
-            tweet_full = json.loads(line)
-            tweets.append({
-                'id': tweet_full['id'],
-                'text': tweet_full['text'].lower(),
-                'label': tweet_full['Annotation'],
-                'name': "TESTE"
-                })
+            tweets = f.readlines()
+        # for line in data:
+        #     tweet_full = json.loads(line)
+        #     tweets.append({
+        #         'id': tweet_full['id'],
+        #         'text': tweet_full['text'].lower(),
+        #         'label': tweet_full['label'],
+        #         'name': "TESTE"
+        #         })
 
     #pdb.set_trace()
     return tweets
@@ -24,19 +24,4 @@ def get_data():
 
 if __name__=="__main__":
     tweets = get_data()
-    males, females = {}, {}
-    with open('./tweet_data/males.txt') as f:
-        males = set([w.strip() for w in f.readlines()])
-    with open('./tweet_data/females.txt') as f:
-        females = set([w.strip() for w in f.readlines()])
-
-    males_c, females_c, not_found = 0, 0, 0
-    for t in tweets:
-        if t['name'] in males:
-            males_c += 1
-        elif t['name'] in females:
-            females_c += 1
-        else:
-            not_found += 1
-    print males_c, females_c, not_found
-    pdb.set_trace()
+    print(tweets)
